273306a1e22f95b596208a350d2c5288291b6da0
alliebia375 <79436822+alliebia375@users.noreply.github.com>
2021-02-25 18:38:36 -0600

    first program

---
+++ forms/Switch/Switch.js
@@ -0,0 +1,12 @@
+/*let firstName = ""
+let state = ""
+let temperature = ""
+
+firstName = prompt("What is your first name?")
+state = prompt("What state are you from? NE or FL?")
+temperature = prompt("what is the temperature?")
+
+switch(temperature state) {
+case '< 32' && 'NE': 
+    console.log(`${firstName}, wear a warm coat, hat, scarf and gloves.`)
+    */
---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,34 @@
+
+//this code declares variables for later input
+let firstName = ""
+let state = ""
+let temperature = ""
+
+//this code makes prompts for users to input
+firstName = prompt("What is your first name?")
+state = prompt("What state are you from? NE or FL?")
+temperature = prompt("what is the temperature?")
+
+//this sets up the arrays
+message = ["wear a warm coat, hat, scarf and gloves.", 
+"wear a warm coat but you won't need a hat, scarf or gloves.", 
+"wear your warmest coat, a warm hat, a scarf, and warm gloves.",
+"wear a warm coat, hat and gloves. Maybe a scarf too."]
+
+if (temperature < 32 && state == 'NE') { 
+    console.log(`${firstName}, ${message[0]}.`)
+}
+else if (temperature <= 50 && temperature >= 32 && state == 'NE') {
+    console.log(`${firstName}, ${message[1]}.`)
+}
+else if (temperature <= 50 && temperature >= 32 && state == 'FL') {
+    console.log(`${firstName}, ${message[2]}.`)
+}
+else if (temperature <= 70 && temperature >= 50 && state == 'FL') {
+    console.log(`${firstName}, ${message[3]}.`)
+}
+else {
+  console.log('Invalid season.');
+}
+
+
