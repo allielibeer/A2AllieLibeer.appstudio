21f39e1885c3e4a7fb64d75e59827872f6ba5b17
alliebia375 <79436822+alliebia375@users.noreply.github.com>
2021-02-28 17:56:31 -0600

    sundayupdate

--- forms/Switch/Switch.js
+++ forms/Switch/Switch.js
@@ -25,53 +25,16 @@ case (state == stateName[0] && temperature <32):
 case (state == stateName[0] && temperature >=32 && temperature <=50):
     console.log(`${firstName}, ${message[1]}.`)
     break;
-case (state == stateName[1] && temperature >=32 temperature <=50):
+case (state == stateName[1] && temperature >=32 && temperature <=50):
     console.log(`${firstName}, ${message[2]}.`)
     break;
-case (state == stateName[1] && temperature >=50 temperature <=70:
+case (state == stateName[1] && temperature >=50 && temperature <=70):
     console.log(`${firstName}, ${message[3]}.`)
-    break;
-default:
-    console.log(`I do not know the temperature.`)
-    break;
-}
-    
-    
-    /*
-if (temperature < 32 && state == 'NE') { 
-    clothingOption = 1
-}
-else if (temperature <= 50 && temperature >= 32 && state == 'NE') {
-    clothingOption = 2
-}
-else if (temperature <= 50 && temperature >= 32 && state == 'FL') {
-    clothingOption = 3
-}
-else if (temperature <= 70 && temperature >= 50 && state == 'FL') {
-    clothingOption = 4
-}
-else {
-  console.log('Invalid input.');
-}
+    break; 
 
-*/
-/*
-//this sets up the swithces 
-switch(clothingOption) {
-case (state == 'NE' && temperature <32) : 
-    console.log(`${firstName}, ${message[0]}`)
-    break;
-case State2':
-    console.log(`${firstName}, ${message[1]}.`)
-    break;
-case '3':
-    console.log(`${firstName}, ${message[2]}.`)
-    break;
-case '4':
-    console.log(`${firstName}, ${message[3]}.`)
-    break;
 default:
     console.log(`I do not know the temperature.`)
     break;
+}
     
-    */
+    
